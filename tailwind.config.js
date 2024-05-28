ee38b5d8d073c95bc890b9b0a4c4accca5bc17f0
Radistian <radistian.azka@gmail.com>
2024-05-28 08:27:21 +0700

    "Updated HTML, CSS, and JavaScript files with various changes to layout, styling, and content."

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,14 @@ module.exports = {
         f4:"#20B2AA",
         f5:"#1C4E2A",
         f6:"#21956C",
+        f7:"#FFBE04",
+        f8:"#66CF8A",
+        f9:"#8FBC8F",
       },
       backgroundImage:{
-        bg1:"url('../images/bgtesti.png')"
+        bg1:"url('../images/bgtesti.png')",
+        bgJus:"url('../images/bgJus.png')",
+        bgProduk:"url('../images/bgProduk.png')",
       }
     },
   },
