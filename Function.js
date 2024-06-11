6148b813ac8b4376270df27145984b1b0fdbcfd0
Radistian <radistian.azka@gmail.com>
2024-06-11 14:14:37 +0700

    11/06/2024 14.14

--- Function.js
+++ Function.js
@@ -1,14 +1,3 @@
-function menu() {
-    if (document.documentElement.scrollTop > 20) {
-        document.getElementById('topHeader').classList.add("bg-white");
-        document.getElementById('topHeader').classList.add("shadow-lg");
-    } else {
-        if (document.documentElement.scrollTop == 0 && !xStatus) {
-            document.getElementById('topHeader').classList.remove('bg-white');
-            document.getElementById('topHeader').classList.remove('shadow-lg');
-        }
-    }
-}
 var xStatus = false;
 
 function showHideMenu() {
@@ -26,5 +15,68 @@ function showHideMenu() {
         xStatus = false;
         document.getElementById('menu').classList.add("hidden");
         document.getElementById('menu').classList.remove("flex");
+    }
+}
+
+// let tag = document.getElementById("tag")
+// tag.addEventListener('mouseenter', function () {
+//     tag.style.width = "30px"
+//     tag.style.height = "30px"
+//     tag.style.backgroundColor = "blue"
+//     tag.style.borderRadius = "5px"
+// })
+// tag.addEventListener('mouseleave', function () {
+//     tag.style.width = "28px"
+//     tag.style.height = "28px"
+//     tag.style.backgroundColor = "transparent"
+
+// })
+
+function like() {
+    const like = document.getElementById('like').classList.toggle('like')
+    console.log(like);
+    if (like==true) {
+        document.getElementById('like').classList.remove('bx-heart')
+        document.getElementById('like').classList.add('bxs-heart')
+    }
+    else{
+        document.getElementById('like').classList.remove('bxs-heart')
+        document.getElementById('like').classList.add('bx-heart')
+    }
+}
+function like2() {
+    const like = document.getElementById('like2').classList.toggle('like2')
+    console.log(like);
+    if (like==true) {
+        document.getElementById('like2').classList.remove('bx-heart')
+        document.getElementById('like2').classList.add('bxs-heart')
+    }
+    else{
+        document.getElementById('like2').classList.remove('bxs-heart')
+        document.getElementById('like2').classList.add('bx-heart')
+    }
+}
+function like3() {
+    const like = document.getElementById('like3').classList.toggle('like3')
+    console.log(like);
+    if (like==true) {
+        document.getElementById('like3').classList.remove('bx-heart')
+        document.getElementById('like3').classList.add('bxs-heart')
+    }
+    else{
+        document.getElementById('like3').classList.remove('bxs-heart')
+        document.getElementById('like3').classList.add('bx-heart')
+    }
+}
+function like4() {
+    const like = document.getElementById('like4').classList.toggle('like4')
+    console.log(like);
+    if (like==true) {
+        document.getElementById('like4').classList.remove('bx-heart')
+        document.getElementById('like4').classList.add('bxs-heart')
+    }
+    else{
+        document.getElementById('like4').classList.remove('bxs-heart')
+        document.getElementById('like4').classList.add('bx-heart')
     }
 }
